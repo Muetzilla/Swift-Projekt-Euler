6e16db64817220a9e2b8027eb41a61120e5e6dd9
Muetzilla <moritz.gasbichler@ubs.com>
2022-07-12 16:05:26 +0200

    Test for problem solution

--- Project Euler/Problems/Problem1.swift
+++ Project Euler/Problems/Problem1.swift
@@ -33,7 +33,7 @@ struct Problem1: View {
 }
 
 
-func calculateSumOfProdukts() -> Int{
+public func calculateSumOfProdukts() -> Int{
     var sum = 0
     for i in 1...999{
         if(i % 3 == 0 || i % 5 == 0){
---
+++ Project EulerTests/Projekt_Euler Problem Result Tests.swift
@@ -0,0 +1,35 @@
+import XCTest
+@testable import Project_Euler
+
+class Project_EulerTests: XCTestCase {
+    func testProblem1ForResult(){
+        XCTAssertEqual(calculateSumOfProdukts(), 233168)
+    }
+//    func testProblem2ForResult(){
+//        XCTAssertEqual(Util.getProductFromString(string: "1234 Hello World"), 1)
+//    }
+//    func testProblem3ForResult(){
+//        XCTAssertEqual(Util.getProductFromString(string: "0000125"), 10)
+//    }
+//    func testProblem4ForResult(){
+//        XCTAssertEqual(Util.getProductFromString(string: "0000125"), 10)
+//    }
+//    func testProblem5ForResult(){
+//        XCTAssertEqual(Util.getProductFromString(string: "0000125"), 10)
+//    }
+//    func testProblem6ForResult(){
+//        XCTAssertEqual(Util.getProductFromString(string: "0000125"), 10)
+//    }
+//    func testProblem7ForResult(){
+//        XCTAssertEqual(Util.getProductFromString(string: "0000125"), 10)
+//    }
+//    func testProblem8ForResult(){
+//        XCTAssertEqual(Util.getProductFromString(string: "0000125"), 10)
+//    }
+//    func testProblem9ForResult(){
+//        XCTAssertEqual(Util.getProductFromString(string: "0000125"), 10)
+//    }
+//    func testProblem10ForResult(){
+//        XCTAssertEqual(Util.getProductFromString(string: "0000125"), 10)
+//    }
+}
